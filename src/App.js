21105890f1508b5004f018908722c70b05554d5d
qusyairin <azizulqusyairin@gmail.com>
2024-08-16 17:29:42 +0800

    first work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Landing from './components/Landing';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-        <nav className="navbar">
-            <ul className="nav-links">
-                <li>
-                    <a href="#">Home</a>
-                </li>
-            </ul>
-        </nav>
-      <Home/>
+      <header className="navbar">
+          <div className="navbar-container">
+              <div className="logo">
+                  GeoID
+              </div>
+              <nav className="nav-links">
+                  <a href="#home">Home</a>
+              </nav>
+              <div className="login-button">
+                  <span style={{marginRight: '30px', color: 'white', cursor: 'pointer'}}>Upload</span>
+                  <button>Login</button>
+              </div>
+          </div>
+      </header>
+      <Login/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -64,7 +64,7 @@ function Home() {
       <GoogleMap
         apiKey=""
         defaultCenter={{ lat: 3.166, lng: 101.7 }}
-        defaultZoom={5}
+        defaultZoom={9}
         mapMinHeight="100vh"
         onGoogleApiLoaded={onGoogleApiLoaded}
         onChange={(map) => console.log('Map moved', map)}
@@ -80,6 +80,109 @@ function Home() {
             // onDragEnd={(e, { latLng }) => {}}
           >HELLO</Marker>
       </GoogleMap>
+
+       {/* Floating Filter Menu Box */}
+       <div
+        style={{
+          position: "absolute",
+          top: "6rem",
+          right: "5rem",
+          background: "white",
+          padding: "10px 15px",
+          borderRadius: "8px",
+          boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.2)",
+          background: "rgba(42, 157, 143, 0.5)", // Slightly transparent background
+          zIndex: 1000, // Ensure it appears above the map
+        }}
+      >
+
+        <div style={{display: 'flex'}}>
+            <div style={{marginRight: '2rem'}}>
+                <div>
+                <label>
+                    <select className="dropdown">
+                        <option value="" selected>
+                            Country
+                        </option>
+                        <option value="Malaysia">Malaysia</option>
+                    </select>
+                </label>
+                </div>
+
+                <div className="reset-button" style={{ marginTop: '20px'}}>
+                  <button>Reset</button>
+                 </div>
+            </div>
+
+            <div style={{marginRight: '2rem'}}>
+                <div>
+                <label>
+                    <select className="dropdown">
+                        <option value="" selected>
+                            Formation
+                        </option>
+                        <option value="Malaysia">Malaysia</option>
+                    </select>
+                </label>
+                </div>
+
+
+                <div className="login-button" style={{ marginTop: '20px'}}>
+                  <button>Apply Filter</button>
+                 </div>
+            </div>
+
+            <div style={{marginRight: '2rem'}}>
+                <div>
+                <label>
+                    <select className="dropdown">
+                        <option value="" selected>
+                            Age
+                        </option>
+                        <option value="Malaysia">Malaysia</option>
+                    </select>
+                </label>
+                </div>
+
+
+                <div style={{marginTop: '20px'}}>
+                <label>
+                    <select className="dropdown">
+                        <option value="" selected>
+                            Type of Rock
+                        </option>
+                        <option value="Malaysia">Malaysia</option>
+                    </select>
+                </label>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                <label>
+                    <select className="dropdown">
+                        <option value="" selected>
+                            Major Lithology
+                        </option>
+                        <option value="Malaysia">Malaysia</option>
+                    </select>
+                </label>
+                </div>
+
+
+                <div style={{marginTop: '20px'}}>
+                <label>
+                    <select className="dropdown">
+                        <option value="" selected>
+                            Fossil
+                        </option>
+                        <option value="Malaysia">Malaysia</option>
+                    </select>
+                </label>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
